83e92663b3c5f3511dfbdc2117a37a3f10f461ff
praveenkumardev-tech <tamizhprem70@gmail.com>
2025-06-28 17:11:55 +0530

    route structure

--- main.js
+++ main.js
@@ -7,6 +7,30 @@ app.get('/', (req, res) => {
 
 });
 
+//CRUD functionalities of movies
+
+//R - For reading movie
+app.get('/movies', () => {
+
+})
+
+
+//C - For creating movies
+app.post('/movies', () => {
+
+})
+
+
+//U - for updating movies
+app.put('/movies/:id', () => {
+
+})
+
+//D- For Deleting movie
+app.delete('/movies/:id', () => {
+  
+})
+
 app.listen(PORT, () => {
   console.log(`The server is running at http://localhost:${PORT}`)
 });
